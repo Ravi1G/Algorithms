b5bd16654eee9f46ceb2f70a78e5771e2e1c3dae
Conservant <aomitenev@gmail.com>
2013-11-26 12:16:08 +0400

    binar tree search

--- BinarSearchTree/binarsearchtree.h
+++ BinarSearchTree/binarsearchtree.h
@@ -3,75 +3,153 @@
 
 #include "node.h"
 
-
+//template <class TYPE>
 class BinarSearchTree
 {
     private:
-        TreeNode *Root; // pointer to root of tree;
 
-        void InOrder(TreeNode *ptr)
-        {
-            if (ptr != NULL){
-                InOrder(ptr->Left);
-                std::cout << ptr->Data << " ";
-                InOrder(ptr->Right);
-            }
-        }
+
+        TreeNode *Root; // pointer to root of tree
+
+        void InOrder(TreeNode *);
+        void PreOrder(TreeNode *);
+        void PostOrder(TreeNode *);
+
     public:
-        int ShowRoot(){
-            return Root->Data;
 
-        }
+        BinarSearchTree();
 
-        BinarSearchTree()
-        {
-            Root = NULL;
-        }
+        void Insert(int, int);
 
-        void add(int x)
-        {
-            TreeNode *ptr = new TreeNode(x),
-                     *cur;
-
-
-            // if tree is empty
-            if (Root == NULL){
-                Root = ptr;
-                return;
-            }
-            // else
-            cur = Root;
-
-            if (x < cur->Data){
-                cur->Left = ptr;
-            }
-
-            if (x > cur->Data)
-                cur->Right = ptr;
-
-        }
-
-
-        void InOrderWalk()
-        {
-            InOrder(Root);
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
+        void InOrderWalk();
+        void PreOrderWalk();
+        void PostOrderWalk();
 
 
 };
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+//template <class TYPE>
+BinarSearchTree::BinarSearchTree()
+{
+    Root = NULL;
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+//template <class TYPE>
+void BinarSearchTree::InOrderWalk()
+{
+    InOrder(Root);
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+void BinarSearchTree::PreOrderWalk()
+{
+    PreOrder(Root);
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+void BinarSearchTree::PostOrderWalk()
+{
+    PostOrder(Root);
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+//template <class TYPE>
+void BinarSearchTree::InOrder(TreeNode *tr)
+{
+    if (tr){
+        InOrder(tr->Left);
+        std::cout << tr->Data << " ";
+        InOrder(tr->Right);
+    }
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+void BinarSearchTree::PreOrder(TreeNode *tr)
+{
+    if (tr){
+        std::cout << tr->Data << " ";
+        InOrder(tr->Left);
+        InOrder(tr->Right);
+    }
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+void BinarSearchTree::PostOrder(TreeNode *tr)
+{
+    if (tr){
+        InOrder(tr->Left);
+        InOrder(tr->Right);
+        std::cout << tr->Data << " ";
+    }
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+//template <class TYPE>
+void BinarSearchTree::Insert(int key, int val)
+{
+    TreeNode *ptr, *cur, *par;
+
+    ptr = new TreeNode(key, val); // выделение памяти для нового узла
+
+    if (Root == NULL){ // if tree is empty
+        Root = ptr;
+        return;
+    }
+
+    cur = Root;
+    par = NULL;
+
+    while (cur){
+        par = cur;
+
+        if (val < cur->Data)
+            cur = cur->Left;
+        else
+            cur = cur->Right;
+    }
+
+    ptr->Parent = par;
+
+    if(val < par->Data)
+        par->Left = ptr;
+    else
+        par->Right = ptr;
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- BinarSearchTree/node.h
+++ BinarSearchTree/node.h
@@ -1,49 +1,43 @@
 #ifndef NODE_H_
 #define NODE_H_
 
+
+//template <class TYPE>
 class TreeNode
 {
     private:
         friend class BinarSearchTree;
-        //char Key;
+        int Key;
         int Data;
         TreeNode *Parent,
-             *Left,
-             *Right;
+                       *Left,
+                       *Right;
+
 
     public:
-    TreeNode()
-    {
-        Parent = Left = Right = NULL;
-    }
+    TreeNode();
+    TreeNode(int key, int val);
 
-    TreeNode(int x)
-    {
-        Parent = Left = Right = NULL;
-        Data = x;
-    }
-
-
-
-/*
-    Node(int k, int d)
-    {
-        Key = k;
-        Data = d;
-
-        Parent = Left = Right = NULL;
-    }
-*/
 };
 
+//////////////////////////////////////////////
+//////////////////////////////////////////////
 
+//template <class TYPE>
+TreeNode::TreeNode()
+    {
+        Parent = Left = Right = NULL;
+    }
 
+//////////////////////////////////////////////
+//////////////////////////////////////////////
 
-
-
-
-
-
-
+//template <class TYPE>
+TreeNode::TreeNode(int key, int val)
+    {
+        Parent = Left = Right = NULL;
+        Data = val;
+        Key = key;
+    }
 
 #endif // NODE_H_
