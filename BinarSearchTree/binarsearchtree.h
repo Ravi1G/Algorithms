4b2f22a6152ebe942f99a55d0d06862ed9a128c7
Conservant <aomitenev@gmail.com>
2014-01-14 17:22:57 +0400

    add-on BTS

--- BinarSearchTree/binarsearchtree.h
+++ BinarSearchTree/binarsearchtree.h
@@ -17,11 +17,14 @@ class BinarSearchTree
 
         TreeNode* SearchTree(TreeNode *, int *);
 
+
+
     public:
 
         BinarSearchTree();
 
         void Insert(int, int);
+        void DeleteNode(TreeNode *);
         void Delete(int);
 
         void InOrderWalk();
@@ -150,15 +153,172 @@ void BinarSearchTree::Insert(int key, int val)
 
 void BinarSearchTree::Delete(int key)
 {
-    /*
+
+    TreeNode *f = Search(key);
+    std::cout << "\nNow deleting elem with key " << key <<". It contains " << f->data() << std::endl;
+    DeleteNode(f);
+
+    //f = NULL;
+
+/*
     3 ситуации:
     а) у узла нет дочерних узлов
     б) 1 дочерний узел
     в) 2 дочерних узла
-    */
+
+*/
 
 
 
+}
+
+///////////////////////////////////////////
+///////////////////////////////////////////
+
+TreeNode* BinarSearchTree::DeleteNode(TreeNode *delNode)
+{
+
+    // при удалении узла из дерева возможны 3 ситуации:
+    // а) у узла нет потомков. тогда все просто.
+    // б) у узла один дочерний узел (правый или левый), тогда все тоже просто.
+    // в) у узла 2 дочерних узла. тут будем думать.
+
+    //а)
+/*/
+    TreeNode *par = del->Parent;
+
+    std::cout << "\n parent is "<< par->Key << ":" << par->Data;
+    if ((del->Left == NULL) && (del->Right == NULL)){
+        if (par->Left == del)
+            par->Left = NULL;
+        else
+            par->Right = NULL;
+
+
+        delete del;
+        del == NULL;
+    }
+
+    //б)
+    //б1) нет левого дочернего узла
+
+    else if (del->Left == NULL){
+        if (par->Left == del)
+            par->Left = del->Right;
+        else
+            par->Right = del->Right;
+    }
+
+
+    //б2) нет правого дочернего узла
+    else if (del->Right == NULL){
+        if (par->Left == del)
+            par->Left = del->Left;
+        else
+            par->Right = del->Left;
+    }
+
+    //в)
+
+    else{
+
+    }
+
+//*/
+/*
+
+    TreeNode *par = del->Parent;
+
+    if (!del->Left && !del->Right)
+    {
+
+        if (par->Left == del)
+            par->Left = NULL;
+        else
+            par->Right = NULL;
+        //delete del;
+
+        //
+        //del = NULL;
+        //delete del;
+        return;
+
+    }
+//*/
+
+    // ищем cur - элемент убираемый путем "склейки" родителя и потомка // узел на подмену
+    // a)cur = del  -  сам узел (если у узла не более одного дочернего узла,
+                                      //т.е. либо один узел, либо ни одного.).
+
+    // б)кур = следующий за удаляемым (у узла нет левой дочки)
+    //- если у узла 2 дочерних узла.
+
+    // вариант кормена
+
+
+    TreeNode *changeNode, *cur = NULL;
+
+    if(!delNode->Left || !delNode->Right)
+        changeNode = delNode;  //узел на подмену - сам удаляемый узел.
+    else
+        changeNode = Successor(del); // у узла 2 доч.узла=> узел на подмену следующий в симметричном обходе.
+
+
+    if (changeNode->Left)
+        cur = changeNode->Left;
+    else
+        cur = changeNode->Right;
+
+    // определив если ли у
+
+    if (cur)
+        x->Parent = changeNode->Parent;
+
+    if (!changeNode->Parent)
+        Root = x;
+    else if (changeNode == changeNode->Parent->Left)
+        changeNode->Parent->Left = x;
+    else
+        changeNode->Parent->Right = x;
+
+
+    if (changeNode != delNode){
+        del->Key = changeNode->Key;
+        del->Data = changeNode->Data;
+    }
+
+    //delete del;
+    //if(!cur->Left)
+
+
+
+
+    /*
+    TreeNode *par = cur->Parent;
+
+    TreeNode *cur, *x;
+
+    if (!cur->Left || !cur->Right)
+        cur = del;
+    else
+        cur = Successor(del); // подумать над cur = successor(cur);
+
+
+    if (cur->Left != NULL)
+        x = cur->Left;
+    else
+        x = cur->Right;
+
+    if (x != NULL){
+
+
+        if (cur == par->Left)
+            par->Left = NULL;
+        if (cur == par->Right)
+            par->Right = NULL;
+        delete cur;
+    }
+*/
 
 }
 
